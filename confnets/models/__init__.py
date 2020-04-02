4e94a91c93a9805f0e94ad623c4272de89d5c841
Alberto Bailoni <bailoni.alberto@gmail.com>
2020-04-02 13:04:28 +0200

    Update __init__.py

--- confnets/models/__init__.py
+++ confnets/models/__init__.py
@@ -1,3 +1,3 @@
 from .unet import UNet3d, UNet2d, SuperhumanSNEMINet, IsotropicSuperhumanSNEMINet
 from .localnet import LocalNet
-from .generalized_unet import MultiScaleInputMultiOutput3DUNet
+from .generalized_unet import MultiScaleInputMultiOutput3DUNet, MultiScaleInput3DUNet
